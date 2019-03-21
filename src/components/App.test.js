482e54884dc84384c534075ea4556862f08d64be
tinluong28 <tinluong28@gmail.com>
2019-03-20 23:01:50 -0700

    first commit

---
+++ src/components/App.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+
+const app = shallow(<App />);
+
+it("render correctly", () => {
+  expect(app).toMatchSnapshot();
+});
+
+it("initializes the `state` with an empty list of gifts", () => {
+  expect(app.state().gifts).toEqual([]);
+});
+
+it("add a new gift to `state` when clicking the `add gift` button", () => {
+  app.find(".btn-add").simulate("click");
+
+  expect(app.state().gifts).toEqual([{ id: 1 }]);
+});
